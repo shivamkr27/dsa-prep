1ada054f31bdd03b575ccd29d2086304b55503c8
shivam <148871839+shivamkr27@users.noreply.github.com>
2026-07-17 18:11:23 +0530

    Time: 4 ms (92.78%), Space: 44 MB (87.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,38 +1,38 @@
 class Solution {
     public String reverseWords(String s) {
-        char[] arr = s.toCharArray();
-        int n = arr.length;
-        
-        int left = 0, right = n - 1;
-        while (left < right) {
-            char temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left++;
-            right--;
+        int left=0;
+        char[] arr=s.toCharArray();
+        int n=arr.length;
+        int right=n-1;
+        //swap
+        while(left<right){
+        char temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        left++;
+        right--;
         }
-        
-        StringBuilder result = new StringBuilder();
-        int start = 0;
-        
-        for (int i = 0; i <= n; i++) {
-            if (i == n || arr[i] == ' ') {
-                if (start != i) {
-                    int l = start, r = i - 1;
-                    while (l < r) {
-                        char temp = arr[l];
-                        arr[l] = arr[r];
-                        arr[r] = temp;
-                        l++;
-                        r--;
-                    }
-                    if (result.length() > 0) result.append(" ");
-                    result.append(arr, start, i - start);
-                }
-                start = i + 1;
-            }
-        }
-        
-        return result.toString();
+        StringBuilder result =new StringBuilder();
+int start=0;
+for(int i=0;i<=n;i++){
+if(i==n || arr[i]== ' '){
+if(start!=i){
+int l=start;
+int r=i-1;
+while(l<r){
+    char temp=arr[l];
+    arr[l]=arr[r];
+    arr[r]=temp;
+    l++;
+r--;
+}
+if(result.length()>0) result.append(" ");
+result.append(arr,start,i-start);
+}
+start=i+1;
+}}
+return result.toString();
+
+
     }
 }
